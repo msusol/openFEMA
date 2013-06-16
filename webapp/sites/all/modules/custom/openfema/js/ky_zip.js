2d5ffcccac685e2c8a4447427cf82e1e16e5a007
mark.susol <Mtmp9sus>
2013-06-16 18:31:40 -0400

    first working fire rate chart in D3

--- webapp/sites/all/modules/custom/openfema/js/ky_zip.js
+++ webapp/sites/all/modules/custom/openfema/js/ky_zip.js
@@ -3,85 +3,116 @@
     // get variables from drupal
     var openfema = Drupal.settings.openfema;
 
-    var width = 960,
-        height = 500;
+    var Chart = function (data) {
 
-    var color = d3.scale.threshold()
-        .domain([30, 60, 120, 360])
-        .range(["#ffffcc","#c2e699","#78c679","#31a354","#006837"]);
+        var height = 500;
+        var width  = 860;
 
-// A position encoding for the key only.
-    var x = d3.scale.linear()
-        .domain([0, 390])
-        .range([0, 240]);
+        var projection = d3.geo.albersUsa().scale(6000);
+        var coords = projection(['-85.50', '37.80']);     // sudo: KY specific
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .tickSize(13)
-        .tickValues(color.domain());
+        var svg = d3.select('#geo-map').append('svg')
+            .attr('width', width)
+            .attr('height', height);
 
-// Kentucky State Plane, Single Zone
-// ftp://kygeonet.ky.gov/kygeodata/standards/Ky_StatePlane.pdf
-    var projection = d3.geo.conicConformal()
-        .rotate([85 + 45 / 60, -36 - 20 / 60])
-        .parallels([37 + 05 / 60, 38 + 40 / 60])
-        .scale(7090)
-        .center([-0.04940, 1.50406])
-        .translate([width / 2, height / 2]);
+        var group = svg.append('g')
+            .attr('transform', 'translate(' + (-coords[0]+width/2) + ',' + (-coords[1]+height/2) + ')');
 
-    var path = d3.geo.path()
-        .projection(projection);
+        d3.json(openfema.data_path + "us-states.json", function(json) {
+            group.selectAll('path')
+                .data(json.features)
+                .enter().append('path')
+                .attr('d', d3.geo.path().projection(projection))
+                .attr('id', function(d){
+                    return d.properties.code // state abbreviation
+                })
+                .style('fill', function(d){
+                    if (d.properties.code == 'KY') {      // sudo: KY specific
+                        return '#ccc'
+                    } else {
+                        return '#eee';
+                    }
+                })
+                .style('stroke', 'white')
+                .style('stroke-width', 1);
+        });
 
-    var svg = d3.select("container").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        // Mark Labels and Symbols, append a new grouping so on top of map
+        var rScale = d3.scale.pow()
+            .domain([0, d3.max(data,function(d){return d.properties.totalFires;})])
+            .range([0,15]);
 
-    var g = svg.append("g")
-        .attr("class", "key")
-        .attr("transform", "translate(40,40)");
+        var labels = svg.append('g')
+            .attr('transform', 'translate(' + (-coords[0]+width/2) + ',' + (-coords[1]+height/2) + ')');
 
-    g.selectAll("rect")
-        .data(color.range().map(function(d, i) {
-            return {
-                x0: i ? x(color.domain()[i - 1]) : x.range()[0],
-                x1: i < color.domain().length ? x(color.domain()[i]) : x.range()[1],
-                z: d
-            };
-        }))
-        .enter().append("rect")
-        .attr("height", 8)
-        .attr("x", function(d) { return d.x0; })
-        .attr("width", function(d) { return d.x1 - d.x0; })
-        .style("fill", function(d) { return d.z; });
+        labels.selectAll('symbol')
+            .data(data)
+            .enter().append('circle')
+            .attr('class', 'symbol')
+            .attr('cx', function(d){
+                var c = projection(d.geometry.coordinates);
+                return c[0];
+            })
+            .attr('cy', function(d){
+                var c = projection(d.geometry.coordinates);
+                return c[1];
+            })
+            .attr('r', function(d) {
+                return rScale(d.properties.totalFires);
+            })
+            .style('fill', 'red')
+            .style('opacity', 0.4);
 
-    g.call(xAxis).append("text")
-        .attr("class", "caption")
-        .attr("y", -6)
-        .text("Population per square mile");
+        if (0) {
+            labels.selectAll("text")
+                .data(data)
+                .enter().append("svg:text")
+                .text(function(d){
+                    return d.properties.label;
+                })
+                .attr("class", "labels")
+                .attr("x", function(d){
+                    var c = projection(d.geometry.coordinates);
+                    return c[0];
+                })
+                .attr("y", function(d){
+                    var c = projection(d.geometry.coordinates);
+                    return c[1];
+                });
+        }
 
-    d3.json(openfema.data_path + "ky-counties.json", function(error, ky) {
-        var counties = topojson.feature(ky, ky.objects.counties);
+    };
 
-        svg.append("g")
-            .attr("class", "county")
-            .selectAll("path")
-            .data(counties.features)
-            .enter().append("path")
-            .attr("d", path)
-            .style("fill", function(d) { return color(d.density = d.properties.population / d.properties.area); })
-            .append("title")
-            .text(function(d) { return d.properties.name + ": " + d.density.toFixed(0) + "/mi.²"; });
+    var FireData = {
 
-        svg.append("path")
-            .datum(topojson.mesh(ky, ky.objects.counties, function(a, b) { return a !== b; }))
-            .attr("class", "county-border")
-            .attr("d", path);
+        getData: function(){
+            var self = this;
 
-        svg.append("path")
-            .datum(topojson.mesh(ky, ky.objects.counties, function(a, b) { return a === b; }))
-            .attr("class", "state-border")
-            .attr("d", path);
-    });
+            $.ajax({
+                async: false,
+                url: '/openfema_zip_query',
+                data: {'state':'KY','zips':['40003','40865','41014']},
+                dataType: "json",
+                success: function(results) {
+                    debugger;
+                    if ( results['error'] ) {
+                        self.error = results['error'];
+                    } else {
+                        self.results = results;
+                    }
+                },
+                error: function (err) {
+                    self.error = err.responseText;
+                }
+            });
+
+        }
+
+    };
+
+    //FireData.getData();
+
+    Chart(openfema.data);
+
 
 })(jQuery);
